a224f830fff6f27b15f7c0905952d3b1041b83e2
oyefunkeoyesiji <oyefunkeoyesiji26@gmail.com>
2022-12-12 02:35:50 -0800

    My first line of programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /*
  * main -print a line of code using puts
  *
- * Return: O
+ * Return: 0
 */
 int main(void)
 {
       puts("\"Programming is like building a multilingual puzzle");
-      return (O);
+      return (0);
 }      
